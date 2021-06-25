f4705b85015b7909fada089ce5e2c10e257f6041
ktnrg45 <ktnrg45@gmail.com>
2021-06-25 21:34:58 +0000

    Add setup.py

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# pyremoteplay
+# pyremoteplay #
+
+## About ##
+This project provides an API to connect to Remote Play hosts (PS4, PS5). It is not intended to be completely usable and is missing features such as real-time video streaming, although these features may be added in the future. This library is heavily based on the C/C++ project [Chiaki](https://github.com/thestr4ng3r/chiaki).
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""Setup for pyremoteplay."""
+
+from setuptools import find_packages, setup
+
+VERSION = "0.0.2"
+
+MIN_PY_VERSION = "3.8"
+
+REQUIRES = list(open('requirements.txt'))
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Environment :: Console :: Curses',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.8',
+    'Topic :: Games/Entertainment',
+    'Topic :: Home Automation',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: System :: Hardware',
+]
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name='pyremoteplay',
+      version=VERSION,
+      description='Remote Play Library',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      author='ktnrg45',
+      author_email='ktnrg45@github.com',
+      packages=find_packages(exclude=['tests']),
+      url='https://github.com/ktnrg45/pyps4-2ndscreen',
+      license='GPLv3',
+      classifiers=CLASSIFIERS,
+      keywords='playstation sony ps4 ps5 remote play remoteplay rp',
+      install_requires=REQUIRES,
+      python_requires='>={}'.format(MIN_PY_VERSION),
+      test_suite='tests',
+      include_package_data=True,
+      entry_points={"console_scripts": ["pyremoteplay = pyremoteplay.__main__:main"]}
+      )
