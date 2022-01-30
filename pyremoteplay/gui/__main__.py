1ce20aa733d7ff66591ba46c07ad294c6d45f54a
ktnrg45 <ktnrg45@gmail.com>
2022-01-30 16:09:10 -0700

    Move gui main to __main__.py

---
+++ pyremoteplay/gui/__main__.py
@@ -0,0 +1,23 @@
+"""GUI Main Methods for pyremoteplay."""
+import sys
+
+from PySide6 import QtCore, QtWidgets
+
+from .main_window import MainWindow
+
+
+def main():
+    """Run GUI."""
+    QtWidgets.QApplication.setHighDpiScaleFactorRoundingPolicy(
+        QtCore.Qt.HighDpiScaleFactorRoundingPolicy.Floor
+    )
+    app = QtWidgets.QApplication([])
+    app.setApplicationName("PyRemotePlay")
+    widget = MainWindow(app)
+    widget.resize(800, 600)
+    widget.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -14,50 +14,50 @@ with open(version_path, encoding="utf-8") as fp:
 VERSION = version_data["VERSION"]
 MIN_PY_VERSION = version_data["MIN_PY_VERSION"]
 
-REQUIRES = list(open('requirements.txt'))
-REQUIRES_GUI = list(open('requirements-gui.txt'))
+REQUIRES = list(open("requirements.txt"))
+REQUIRES_GUI = list(open("requirements-gui.txt"))
 
 CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
-    'Environment :: Console',
-    'Environment :: Console :: Curses',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Natural Language :: English',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3.8',
-    'Topic :: Games/Entertainment',
-    'Topic :: Home Automation',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    'Topic :: System :: Hardware',
+    "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Environment :: Console :: Curses",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Games/Entertainment",
+    "Topic :: Home Automation",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: System :: Hardware",
 ]
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
 setup(
-    name='pyremoteplay',
+    name="pyremoteplay",
     version=VERSION,
-    description='Remote Play Library and API',
+    description="Remote Play Library and API",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    author='ktnrg45',
-    author_email='ktnrg45dev@gmail.com',
-    packages=find_packages(exclude=['tests']),
-    url='https://github.com/ktnrg45/pyremoteplay',
-    license='GPLv3',
+    long_description_content_type="text/markdown",
+    author="ktnrg45",
+    author_email="ktnrg45dev@gmail.com",
+    packages=find_packages(exclude=["tests"]),
+    url="https://github.com/ktnrg45/pyremoteplay",
+    license="GPLv3",
     classifiers=CLASSIFIERS,
-    keywords='playstation sony ps4 ps5 remote play remoteplay rp',
+    keywords="playstation sony ps4 ps5 remote play remoteplay rp",
     install_requires=REQUIRES,
     extras_require={"GUI": REQUIRES_GUI},
-    python_requires='>={}'.format(MIN_PY_VERSION),
-    test_suite='tests',
+    python_requires=">={}".format(MIN_PY_VERSION),
+    test_suite="tests",
     include_package_data=True,
     entry_points={
         "console_scripts": [
             "pyremoteplay = pyremoteplay.__main__:main",
         ],
         "gui_scripts": [
-            "pyremoteplay-gui = pyremoteplay.gui.__init__:run [GUI]",
-        ]
-    }
+            "pyremoteplay-gui = pyremoteplay.gui.__main__:main [GUI]",
+        ],
+    },
 )
